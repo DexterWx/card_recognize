89e7bf57e166beb10ba2b3675bd3db84711a4f92
meng.chen <meng.chen@17zuoye.com>
2024-04-23 14:55:37 +0800

    feat: 判断填涂比

--- src/recognition/black_fill.rs
+++ src/recognition/black_fill.rs
@@ -1,6 +1,10 @@
 use imageproc::integral_image::sum_image_pixels;
-
+use imageproc::rect::Rect;
 use crate::{models::{engine_rec::ProcessedImages, rec_result::Value, scan_json::Coordinate}, recognition::engine::Engine};
+
+// use std::env;
+// use imageproc::drawing::draw_filled_rect_mut;
+// use image::Rgb;
 
 pub trait RecBlackFill{
     /// 填涂识别
@@ -9,7 +13,32 @@ pub trait RecBlackFill{
 
 impl RecBlackFill for Engine {
     fn rec_black_fill(&self, img: &ProcessedImages, coordinate: &Coordinate) -> Option<Value>{
-        // sum_image_pixels(integral_image, left, top, right, bottom)
-        None
+        
+        let rect = Rect::at(coordinate.x, coordinate.y).of_size(coordinate.w as u32, coordinate.h as u32);
+        //计算图像中区域所有像素值的和
+        let sum_pixels = sum_image_pixels(
+          &img.integral_gray,
+          rect.left() as u32,
+          rect.top() as u32,
+          (rect.right()-1) as u32,
+          (rect.bottom()-1) as u32
+        )[0];
+        let mean_pixel = sum_pixels / (rect.width() * rect.height()) as i64;
+        let filled_ratio = 1.0 - mean_pixel as f32 / 255f32;
+        // println!("====={:?}所在区域填涂比{}=====", coordinate,filled_ratio);     
+
+        // 绘制要识别的填涂区域
+        // let mut img_rgb_painting = img.rgb.clone();
+        // draw_filled_rect_mut(
+        //     &mut img_rgb_painting,   
+        //     Rect::at(coordinate.x, coordinate.y).of_size(coordinate.w as u32, coordinate.h as u32),    
+        //     Rgb([255u8, 0u8, 0u8]),  
+        // );
+
+        // let cwd = env::current_dir().unwrap();
+        // let str = format!("dev/test_data/rec_black_fill/{}_{}_{}_{}.jpg", coordinate.x,coordinate.y,coordinate.w,coordinate.h);
+        // let path = cwd.join(str);
+        // img_rgb_painting.save(path).unwrap();
+        return Some(Value::Float(filled_ratio));
     }
 }
